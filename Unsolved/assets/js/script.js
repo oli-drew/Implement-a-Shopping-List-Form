4b07916d09cf97397e8a2b690c6e6bf5ab59a73c
Oli Drew <oli.drew92@gmail.com>
2022-01-11 20:54:03 +0000

    Task complete

--- Unsolved/assets/js/script.js
+++ Unsolved/assets/js/script.js
@@ -1,6 +1,13 @@
-var shoppingFormEl = $('#shopping-form');
-var shoppingListEl = $('#shopping-list');
+const shoppingFormEl = $("#shopping-form");
+const shoppingListEl = $("#shopping-list");
+const userInput = $("#shopping-input");
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+const submission = () => {
+  event.preventDefault();
+  $("<li>").appendTo(shoppingListEl).text(userInput.val());
+  userInput.val("");
+};
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.submit(submission);
